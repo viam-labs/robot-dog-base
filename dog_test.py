ed3753a968190899fa09b99152c44255e3dea75b
JessamyT <75634662+JessamyT@users.noreply.github.com>
2023-01-30 13:15:37 -0800

    Create dog_test.py

---
+++ dog_test.py
@@ -0,0 +1,19 @@
+# dog_test.py is for testing the connection
+
+import socket, time
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("10.0.0.144", 5001))
+
+cmd = "CMD_MOVE_FORWARD#50"
+s.send(cmd.encode("utf-8"))
+time.sleep(3)
+cmd = "CMD_MOVE_FORWARD#80"
+s.send(cmd.encode("utf-8"))
+time.sleep(3)
+cmd = "CMD_MOVE_FORWARD#25"
+s.send(cmd.encode("utf-8"))
+time.sleep(3)
+cmd = "CMD_MOVE_FORWARD#35"
+s.send(cmd.encode("utf-8"))
+time.sleep(3)
