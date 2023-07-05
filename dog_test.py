e3c927ca958436c9318456f4e0f5c14aa19ecd9b
JessamyT <75634662+JessamyT@users.noreply.github.com>
2023-07-05 12:03:49 -0700

    Adjust time and speed

--- dog_test.py
+++ dog_test.py
@@ -5,9 +5,9 @@ import socket, time
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("PASTE DOG IP ADDRESS HERE", 5001))
 
-cmd = "CMD_MOVE_FORWARD#8"
+cmd = "CMD_MOVE_FORWARD#15"
 s.send(cmd.encode("utf-8"))
-time.sleep(8)
+time.sleep(7)
 cmd = "CMD_MOVE_STOP"
 s.send(cmd.encode("utf-8"))
 cmd = "CMD_RELAX"
